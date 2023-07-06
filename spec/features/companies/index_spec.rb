6ef291c17efd8ebb05664d6cee351fd4a6cce338
Weston Schutt <wtschutt@gmail.com>
2023-07-06 11:52:38 -0600

    Feature: User Story 1 - Parent Index

---
+++ spec/features/companies/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Companies Index', type: :feature do
+  # For each parent table
+  # As a visitor
+  # When I visit '/parents'
+  # Then I see the name of each parent record in the system
+  describe "When I visit '/parents'" do
+    it 'displays the name of each company' do
+      company = Company.create!(name: "Frank & Roze", federal_ein: 123456789, non_profit: false, address_line_1: "4097 E 9th Ave", address_line_2: "", city: "Denver", state: "CO", zipcode: "80220")
+      company2 = Company.create!(name: "Squeeze", federal_ein: 124365879, non_profit: true, address_line_1: "875 Albioni St", address_line_2: "", city: "Denver", state: "CO", zipcode: "80220")
+      visit "/companies"
+
+      expect(page).to have_content(company.name)
+      expect(page).to have_content(company2.name)
+      save_and_open_page
+    end
+  end
+end
