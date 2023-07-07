85b2552c80f002cf6238107f14b157a8de75ca1a
Weston Schutt <wtschutt@gmail.com>
2023-07-07 14:33:08 -0600

    Fix: Update employee index tests and view to accomodate association with Company

--- spec/features/employees/index_spec.rb
+++ spec/features/employees/index_spec.rb
@@ -9,41 +9,43 @@ RSpec.describe "Employees Index Page", type: :feature do
   describe "When I visit '/employees'" do
     describe "I see each Employee in the system including the Employee's attributes" do
       before :each do
-        @manila = Employee.create!(first_name: "Manila", last_name: "Luzon", i9_eligible: true, benefits_eligible: false, salary: 69000)
-        @latrice = Employee.create!(first_name: "Latrice", last_name: "Royale", i9_eligible: true, benefits_eligible: false, salary: 95000)
-        @jimbo = Employee.create!(first_name: "Jimbo", last_name: "Clown", i9_eligible: true, benefits_eligible: false, salary: 87340)
+        @company = Company.create!(name: "Frank & Roze", federal_ein: 123456789, non_profit: false, address_line_1: "4097 E 9th Ave", address_line_2: "", city: "Denver", state: "CO", zipcode: "80220")
+        @company2 = Company.create(name: "Squeeze", federal_ein: 124365879, non_profit: true, address_line_1: "875 Albioni St", address_line_2: "apt i", city: "Denver", state: "CO", zipcode: "80220")
+        @manila = Employee.create!(first_name: "Manila", last_name: "Luzon", i9_eligible: true, benefits_eligible: false, salary: 69000, company_id: @company.id)
+        @latrice = Employee.create!(first_name: "Latrice", last_name: "Royale", i9_eligible: true, benefits_eligible: false, salary: 95000, company_id: @company.id)
+        @jimbo = Employee.create!(first_name: "Jimbo", last_name: "Clown", i9_eligible: true, benefits_eligible: false, salary: 87340, company_id: @company2.id)
+
+        visit "/employees"
       end
 
       it 'shows employees name' do
-        visit "/employees"
-
         expect(page).to have_content(@manila.name)
         expect(page).to have_content(@latrice.name)
         expect(page).to have_content(@jimbo.name)
       end
 
       it 'shows employees i9_eligibility (boolean)' do
-        visit "/employees"
-
         expect(page).to have_content(@manila.i9_eligible)
         expect(page).to have_content(@latrice.i9_eligible)
         expect(page).to have_content(@jimbo.i9_eligible)
       end
 
       it 'shows employees benefits eligibility (boolean)' do
-        visit "/employees"
-
         expect(page).to have_content(@manila.benefits_eligible)
         expect(page).to have_content(@latrice.benefits_eligible)
         expect(page).to have_content(@jimbo.benefits_eligible)
       end
 
       it 'shows employees salary' do
-        visit "/employees"
-
         expect(page).to have_content(@manila.salary)
         expect(page).to have_content(@latrice.salary)
         expect(page).to have_content(@jimbo.salary)
+      end
+
+      it 'shows the employees company id' do
+        expect(page).to have_content(@manila.company_id)
+        expect(page).to have_content(@latrice.company_id)
+        expect(page).to have_content(@jimbo.company_id)
       end
     end
   end
