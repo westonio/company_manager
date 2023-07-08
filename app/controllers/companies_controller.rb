c234f64e2556eaba5a55c22ea1ded12e242f5209
Weston Schutt <wtschutt@gmail.com>
2023-07-08 16:18:51 -0600

    Updated view to edit company info

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -31,5 +31,22 @@ class CompaniesController < ApplicationController
   end
 
   def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    company = Company.find(params[:id])
+    company.update(
+      name: params[:company][:name],
+      federal_ein: params[:company][:federal_ein].to_i,
+      non_profit: params[:company][:non_profit],
+      address_line_1: params[:company][:address_line_1],
+      address_line_2: params[:company][:address_line_2],
+      city: params[:company][:city],
+      state: params[:company][:state],
+      zipcode: params[:company][:zipcode]
+    )
+    company.save
+    redirect_to "/companies/#{company.id}"
   end
 end
