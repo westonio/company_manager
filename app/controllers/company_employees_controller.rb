6b393c4f48664a28b4ef7f654738fabc010b183e
Weston Schutt <wtschutt@gmail.com>
2023-07-10 16:12:28 -0600

    Controller: Update index method to populate sorted employees when url parameter is passed

--- app/controllers/company_employees_controller.rb
+++ app/controllers/company_employees_controller.rb
@@ -1,6 +1,10 @@
 class CompanyEmployeesController < ApplicationController
   def index
     @company = Company.find(params[:id])
-    @employees = Employee.where(company_id: @company.id)
+    if params[:sort] == "A-Z"
+      @employees = Employee.where(company_id: @company.id).order(first_name: :asc)
+    else
+      @employees = Employee.where(company_id: @company.id)
+    end
   end
 end
