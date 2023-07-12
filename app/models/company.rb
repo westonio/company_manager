18ca5d37d0ae9a61b83e8bdda83ec4bdd0cb9ff8
Weston Schutt <wtschutt@gmail.com>
2023-07-11 18:31:57 -0600

    Update Company model with methods to filter related employees

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,12 @@ class Company < ApplicationRecord
   def employee_count
     self.employees.count
   end
+
+  def alphabetical_employees
+    self.employees.order(first_name: :asc)
+  end
+
+  def salary_more_than(amount)
+    self.employees.where("salary > #{amount.to_i}")
+  end
 end
